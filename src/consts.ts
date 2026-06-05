8a95280cff072a781b07fd6b1b9d7baa69b7ba5a
Angie Hu <angie@angiehu.com>
2026-06-06 00:28:29 +0800

    Initial commit: Angie's Blog (Astro)

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Angie's Blog";
+export const SITE_DESCRIPTION = 'Angie 的文字、想法與作品紀錄。';
